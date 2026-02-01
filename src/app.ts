342866d4c4f5fcf730ab6961a51eb94102979f72
Emon Hossain <islamemon59@gmail.com>
2026-02-01 13:18:00 +0600

    student route controller service setup

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,29 @@
 import express from "express";
 import { toNodeHandler } from "better-auth/node";
-import cors from "cors"
+import cors from "cors";
 import { auth } from "./lib/auth";
 import { tutorRouter } from "./modules/tutor/tutor.routes";
 import { adminRouter } from "./modules/admin/admin.routes";
-const app = express()
+import { studentRouter } from "./modules/student/student.routes";
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.APP_URL || "http://localhost:3000",
     credentials: true,
-}))
+  }),
+);
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/api", tutorRouter);
-app.use("/api", adminRouter)
+app.use("/api", adminRouter);
+app.use("/api", studentRouter);
 
-app.get('/', (req, res) => {
-  res.send('Server working perfectly🙂')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Server working perfectly🙂");
+});
 
 export default app;
